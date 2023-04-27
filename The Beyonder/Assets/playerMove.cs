188f379d99208ff778ec5f28b881269c5e9dcdc9
Kooczsi <kooczsi@gmail.com>
2023-04-27 13:41:01 +0200

    added running to the player movement script

--- The Beyonder/Assets/playerMove.cs
+++ The Beyonder/Assets/playerMove.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerMove : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed;
-    // Start is called before the first frame update
+    [SerializeField] private Slider staminaBar;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    private float moveSpeed;
+	// Start is called before the first frame update
+	
 
-    Vector2 movement;
-    // Update is called once per frame
+
+
+	Vector2 movement;
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+            moveSpeed = runSpeed;
+		}
+		else
+		{
+			moveSpeed = walkSpeed;
+		}
     }
 
 	private void FixedUpdate()
